16f64efe94cb2253c8f92ad499803535c0a2e658
y1ungspace <y1ung.space@gmail.com>
2022-05-14 20:31:31 +0200

    feat: add buttons on the page (Sat May 14 2022 16:30:27 GMT+0000)

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('jjjjj')
+let wrapper = document.createElement('DIV');
+wrapper.classList.add('wrapper');
+
+let buttonsBox = document.createElement('DIV');
+buttonsBox.classList.add('buttons-box');
+
+document.body.append(wrapper);
+document.body.append(buttonsBox);
+
+for (let i = 0; i < 6; i++) {
+    let button = document.createElement('DIV');
+    button.classList.add('button', `button${i}`);
+    buttonsBox.append(button);
+}
+
+let memes = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+        
+    }
+]
